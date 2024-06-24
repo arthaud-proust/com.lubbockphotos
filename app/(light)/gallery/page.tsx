ae732218612ab255640d61b7c898a1ebcf9ba7d7
Arthaud <arthaudp33@gmail.com>
2024-06-25 01:19:08 +0200

    style: gallery grid

--- app/(light)/gallery/page.tsx
+++ app/(light)/gallery/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   const photoSets = await getPhotoSets({ count: 500, countPerPhotoSet: 3 })
 
   return (
-    <main className='grid grid-cols-1 items-end gap-4 py-[30vh] pr-4 sm:grid-cols-3 md:grid-cols-4 md:pr-8 lg:grid-cols-6 xl:grid-cols-10'>
+    <main className='grid grid-cols-1 items-end gap-8 py-[30vh] pr-4 sm:grid-cols-3 md:grid-cols-3 md:pr-8 lg:grid-cols-4 xl:grid-cols-6'>
       {photoSets.map((photoSet) => (
         <div key={photoSet.id} className='relative flex-1'>
           <Image
@@ -16,7 +16,10 @@ export default async function Page() {
             height={photoSet.photos[0].small.height}
             alt=''
           />
-          <Link href={`/gallery/${photoSet.id}`} className='block w-full truncate after:absolute after:inset-0'>
+          <Link
+            href={`/gallery/${photoSet.id}`}
+            className='mt-2 block w-full truncate text-center after:absolute after:inset-0'
+          >
             {photoSet.title}
           </Link>
         </div>
